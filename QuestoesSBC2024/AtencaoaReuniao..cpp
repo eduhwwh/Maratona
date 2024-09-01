adac6802ef6436c87cb737bb190421ae9628dd1c
Maria Eduarda <vantev806@gmail.com>
2024-09-01 14:42:12 -0300

    Questao da  maratona

---
+++ QuestoesSBC2024/AtencaoaReuniao..cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+   
+    int n,k;
+
+    cin >> n >> k;
+
+    int n_pausa = n - 1;
+    int calculo = 0;
+    
+    for(size_t i=0 ; i <= k ; i++){
+        calculo = i * n + n_pausa;
+
+        if(calculo > k){
+            calculo = i;
+
+            break;
+        }
+    }
+
+    if(n == 1){
+        cout << k << endl;
+    }else{
+        cout << calculo-1 << endl;
+    }
+
+   
+    return 0;
+
+}
